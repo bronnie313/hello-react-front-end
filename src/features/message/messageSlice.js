1cc30f961cb00a3f69062cf17c41c1c75ebe3144
Bisaso Ronald <bronnie313@gmail.com>
2023-09-30 16:00:33 +0300

    add redux, store, actions and fetch rails api

---
+++ src/features/message/messageSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = "http://localhost:3000/api/v1/messages";
+
+const fetchMessages = createAsyncThunk("messages/fetchMessages", async () => {
+    const response = await axios.get(url);
+    return response.data
+})
+
+const initialState = {
+    messages: {},
+}
+
+
+const messageSlice = createSlice ({
+    name: 'messages',
+    initialState,
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchMessages.fulfilled, (state, action) => {
+            const messageRecieved = action.payload;
+            return { ...state, messages: messageRecieved };
+        })
+    }
+})
+
+export { fetchMessages };
+export default messageSlice.reducer;
